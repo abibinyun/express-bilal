5b74dbe888f830ae82f401581825f3ca6746f40f
bilal <abii.manyun@gmail.com>
2022-09-04 04:38:41 +0700

    update

--- config/mysql.js
+++ config/mysql.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
-require("dotenv").config();
-
-// const connection = mysql.createConnection({
-//   host: "remotemysql.com",
-//   user: "1ZNFMZwwDF",
-//   password: "CmYmkUFzfc",
-//   database: "1ZNFMZwwDF",
-// });
+// require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: "remotemysql.com",
+  user: "1ZNFMZwwDF",
+  password: "CmYmkUFzfc",
+  database: "1ZNFMZwwDF",
 });
+
+// const connection = mysql.createConnection({
+//   host: process.env.HOST,
+//   user: process.env.USERNAME,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
+// });
 
 module.exports = connection;
--- config/sequelize.js
+++ config/sequelize.js
@@ -1,21 +1,21 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
-
-const sequelize = new Sequelize({
-  database: process.env.DATABASE,
-  host: process.env.HOST,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  dialect: process.env.DIALECT,
-});
+// require("dotenv").config();
 
 // const sequelize = new Sequelize({
-//   database: "1ZNFMZwwDF",
-//   host: "remotemysql.com",
-//   username: "1ZNFMZwwDF",
-//   password: "CmYmkUFzfc",
-//   dialect: "mysql",
+//   database: process.env.DATABASE,
+//   host: process.env.HOST,
+//   username: process.env.USERNAME,
+//   password: process.env.PASSWORD,
+//   dialect: process.env.DIALECT,
 // });
+
+const sequelize = new Sequelize({
+  database: "1ZNFMZwwDF",
+  host: "remotemysql.com",
+  username: "1ZNFMZwwDF",
+  password: "CmYmkUFzfc",
+  dialect: "mysql",
+});
 
 (async () => {
   try {
